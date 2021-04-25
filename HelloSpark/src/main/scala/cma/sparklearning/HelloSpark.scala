bdb6c2701b2fb1c8b64358453e84f9ea3d3262e0
Seema Pandey <cma2.mishra@gmail.com>
2021-04-25 19:12:24 +0530

    Initial commit with log4j properties

---
+++ HelloSpark/src/main/scala/cma/sparklearning/HelloSpark.scala
@@ -0,0 +1,16 @@
+package cma.sparklearning
+
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object HelloSpark extends Serializable{
+
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+
+}
